f34f87cf7f19038b7314c2a6f93adf507aca5e00
Ger121190 <gmcabrera121190@gmail.com>
2021-09-12 01:24:22 -0300

    Agrego navbar e imagen de fonto junto con la fuente.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="secondary">
+        <b-navbar-nav class="virtuaula-logo">
+          <div class="virtuaula-box">
+            <img
+              class="virtuaula-img animate__animated animate__bounceInLeft"
+              src="../assets/logo.png"
+              alt="logo"
+              width="100"
+              height="80"
+            />
+            <p class="virtuaula-text animate__animated animate__bounceInRight">
+              VirTuAula
+            </p>
+          </div>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-text class="virtuaula-guest">
+            Guest
+            <b-avatar variant="warning ml-2"></b-avatar>
+          </b-nav-text>
+        </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.virtuaula-box {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 200px;
+  height: 60px;
+}
+
+.virtuaula-text {
+  font-size: 2rem;
+  color: orange;
+  font-family: "Gagalin";
+  padding-left: 5%;
+  margin-bottom: 0;
+  margin-top: 3%;
+  border-left: lightgrey solid 2px;
+}
+
+.virtuaula-img {
+  object-fit: contain;
+}
+
+.virtuaula-guest {
+  display: flex;
+  align-items: center;
+}
+
+/* Mobile */
+
+@media (max-width: 600px) {
+  .virtuaula-text {
+    padding-left: 2%;
+    margin-top: 0;
+  }
+
+  .virtuaula-box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .virtuaula-img {
+    margin-top: 7%;
+  }
+
+  .virtuaula-guest {
+    display: none;
+  }
+
+  .virtuaula-logo {
+    margin: auto;
+  }
+}
+</style>

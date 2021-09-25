84eac742d7aebe52a081724c27dceef0270aaa00
Ger121190 <gmcabrera121190@gmail.com>
2021-09-25 14:42:30 -0300

    Corrijo el tamaño de las tarjetas de las tareas.

--- src/components/CardTask.vue
+++ src/components/CardTask.vue
@@ -2,24 +2,33 @@
   <div
     :class="hide ? 'animate__animated animate__zoomOut animate__faster' : ''"
   >
-    <b-container>
-      <b-row>
-        <b-col>
-          <b-card
-            img-alt="lesson"
-            img-left
-            :title="'¿' + task.statement + '?'"
-            class="mb-3"
-          >
-            <b-form-group>
-              <template v-for="option in task.options">
-                <b-form-radio v-model="selected" :disabled="disabled === 100" :key="option.id" :value="option.id">{{option.responseValue}}</b-form-radio>
-              </template>
-            </b-form-group>
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-col>
+      <b-container>
+        <b-row>
+          <b-col>
+            <b-card
+              img-alt="lesson"
+              img-left
+              :title="task.statement"
+              class="mb-3"
+              style="width: 30rem;"
+            >
+              <b-form-group>
+                <template v-for="option in task.options">
+                  <b-form-radio
+                    v-model="selected"
+                    :disabled="disabled === 100"
+                    :key="option.id"
+                    :value="option.id"
+                    >{{ option.responseValue }}</b-form-radio
+                  >
+                </template>
+              </b-form-group>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-col>
   </div>
 </template>
 
@@ -31,17 +40,20 @@ export default {
       max: 100,
       timer: null,
       selected: this.task.answer,
-      disabled: this.$store.getters.getActualLesson.progress
+      disabled: this.$store.getters.getActualLesson.progress,
     };
   },
   props: ["task", "hide"],
   watch: {
-      selected(newSelected) {
-          if(newSelected) {
-              this.task.answer = this.selected;
-              this.$store.commit("addTaskResponse",{id: this.task.id, answerId: newSelected})
-          }
+    selected(newSelected) {
+      if (newSelected) {
+        this.task.answer = this.selected;
+        this.$store.commit("addTaskResponse", {
+          id: this.task.id,
+          answerId: newSelected,
+        });
       }
+    },
   },
 };
 </script>
